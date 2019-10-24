7871b34f845941d0189b2a9b42d34ad38366819a
Victor Savinov <victor-savinov@users.noreply.github.com>
2019-10-25 02:56:40 +0300

    Satus Core update
    - Added data storage paths
    - Added data deletion function
    - Improved custom event creation

--- core.js
+++ core.js
@@ -4,23 +4,64 @@
 
 const Satus = {
     data: {},
-    events: { get: [], set: [] },
-    
+
+    events: {
+        get: [],
+        set: [],
+        remove: []
+    },
+
     get: function(name, on) {
+        let path = name.split('/').filter(function(value) {
+                return value != '';
+            }),
+            object = this.data;
+
+        for (let i = 0, l = path.length; i < l; i++)
+            object = object[path[i]];
+
         if (on !== false)
             for (let i = 0, l = this.events.get.length; i < l; i++)
                 this.events.get[i](name);
 
-        return this.data[name];
+        return object;
     },
+
     set: function(name, value, on) {
+        let path = name.split('/').filter(function(value) {
+                return value != '';
+            }),
+            object = this.data;
+
+        for (let i = 0, l = path.length; i < l; i++)
+            if (i === l - 1)
+                object[path[i]] = value;
+            else
+                object = object[path[i]];
+
         if (on !== false)
             for (let i = 0, l = this.events.set.length; i < l; i++)
                 this.events.set[i](name, value);
-
-        this.data[name] = value;
     },
+
+    remove: function(name, on) {
+        let path = name.split('/').filter(function(value) {
+                return value != '';
+            }),
+            object = this.data;
+
+        for (let i = 0, l = path.length; i < l; i++)
+            object = object[path[i]];
+
+        if (on !== false)
+            for (let i = 0, l = this.events.remove.length; i < l; i++)
+                this.events.remove[i](name);
+    },
+
     on: function(name, callback) {
+        if (!this.events[name])
+            this.events[name] = [];
+
         this.events[name].push(callback);
     }
 };
