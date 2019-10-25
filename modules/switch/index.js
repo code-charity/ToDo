6ffbb0d1a4a1079368cafdab24cb3600be61701f
Victor Savinov <victor-savinov@users.noreply.github.com>
2019-10-25 03:04:37 +0300

    "switch" module can now use data storage

--- modules/switch/index.js
+++ modules/switch/index.js
@@ -7,21 +7,24 @@
 Satus.switch = function(object) {
     let self = this,
         element = document.createElement('div'),
-        label = object.label;
+        label = object.label,
+        value = Satus.get(object.storage);
 
     element.innerHTML = (label ? '<div class=label>' + label + '</div>' : '') +
         '<div class=container>' +
         ((object.icons || {}).start || '') + '<div class=track><div class=thumb></div></div>' + ((object.icons || {}).end || '') +
         '</div>';
 
-    element.dataset.value = object.value || false;
+    element.dataset.value = value || object.value || false;
 
     element.addEventListener('click', function(event) {
-        if (element.dataset.value == 'true') {
-            element.dataset.value = 'false';
+        if (this.dataset.value == 'true') {
+            this.dataset.value = 'false';
         } else {
-            element.dataset.value = 'true';
+            this.dataset.value = 'true';
         }
+
+        Satus.set(object.storage, this.dataset.value === 'true');
     });
 
     return element;
