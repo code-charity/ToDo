8c6f697f38218b74d333654da31fc98401e158c3
Victor Savinov <victor-savinov@users.noreply.github.com>
2019-10-29 02:09:06 +0300

    Update header.js

--- template/header.js
+++ template/header.js
@@ -9,7 +9,7 @@ const Menu = {
             back: {
                 type: 'button',
                 class: ['satus-button--back'],
-                icon: '<svg xmlns=//www.w3.org/2000/svg viewBox="0 0 24 24" style=width:20px;height:20px><path d="M16.6 3c-.5-.5-1.3-.5-1.8 0l-8.3 8.3a1 1 0 0 0 0 1.4l8.3 8.3a1.2 1.2 0 1 0 1.8-1.7L9.4 12l7.2-7.3c.5-.4.5-1.2 0-1.7z"></path></svg>',
+                icon: '<svg viewBox="0 0 24 24" style=width:20px;height:20px><path d="M16.6 3c-.5-.5-1.3-.5-1.8 0l-8.3 8.3a1 1 0 0 0 0 1.4l8.3 8.3a1.2 1.2 0 1 0 1.8-1.7L9.4 12l7.2-7.3c.5-.4.5-1.2 0-1.7z"></path></svg>',
                 on: {
                     click: function() {
                         document.querySelector('.satus-main__container').close();
@@ -28,7 +28,7 @@ const Menu = {
 
             search: {
                 type: 'button',
-                icon: '<svg xmlns=//www.w3.org/2000/svg viewBox="0 0 24 24"><path d="M15.5 14h-.8l-.3-.3a6.5 6.5 0 1 0-.7.7l.3.3v.8l4.3 4.3a1 1 0 0 0 1.4-1.5L15.5 14zm-6 0a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9z"></path></svg>',
+                icon: '<svg viewBox="0 0 24 24"><path d="M15.5 14h-.8l-.3-.3a6.5 6.5 0 1 0-.7.7l.3.3v.8l4.3 4.3a1 1 0 0 0 1.4-1.5L15.5 14zm-6 0a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9z"></path></svg>',
                 on: {
                     click: function() {
                         let offset = this.offsetLeft - this.offsetWidth / 2;
@@ -143,7 +143,7 @@ const Menu = {
             },
             menu: {
                 type: 'button',
-                icon: '<svg xmlns=//www.w3.org/2000/svg viewBox="0 0 24 24"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg>',
+                icon: '<svg viewBox="0 0 24 24"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg>',
                 on: {
                     click: function() {
                         Satus.dialog({
@@ -161,7 +161,7 @@ const Menu = {
                             export: {
                                 type: 'button',
                                 label: 'Export',
-                                icon: '<svg xmlns=//www.w3.org/2000/svg viewBox="0 0 24 24" style=width:16px;height:16px;margin-right:10px;margin-top:2px><path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/></svg>',
+                                icon: '<svg viewBox="0 0 24 24" style=width:16px;height:16px;margin-right:10px;margin-top:2px><path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/></svg>',
                                 on: {
                                     click: function() {
                                         chrome.runtime.sendMessage({
